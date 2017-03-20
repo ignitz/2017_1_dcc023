d244f9e55d7b7cd52c58bcfe653b23031e9a9cee
ignitz <ignitzhjfk@gmail.com>
2017-03-19 23:12:06 -0300

    first version on python
    first version of an example of sockets in python

--- tp/tp0/client.py
+++ tp/tp0/client.py
@@ -1,15 +1,61 @@
 import socket
-HOST = '127.0.0.1'     # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print 'Para sair use \'q\'\n'
-msg = raw_input()
-while msg <> 'q':
-    tcp.send (msg)
-    msg = raw_input()
-    if msg == 'q':
-        tcp.send (msg)
+import sys
 
-tcp.close()
+HOST = 'localhost'
+PORT = 5000
+
+class Client(object):
+
+    def __init__(self, **kwargs):
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = (
+            kwargs.get('host', HOST),
+            kwargs.get('port', PORT)
+            )
+        try:
+            self.sock.connect(self.address)
+        except:
+            print 'connection refused in %s port %s' % self.address
+            raise
+
+    def receive_data(self, len_data):
+        # receive data from server
+        amount_received = 0
+        amount_expected = len_data
+
+        data = ''
+        while amount_received < amount_expected:
+            data += self.sock.recv(4)
+            amount_received += len(data)
+            print >>sys.stderr, 'received "%s"' % data
+        return data
+
+    def send_data(self, data):
+        # send data throught send method
+        try:
+            print >>sys.stderr, 'sending "%s"' % data
+            self.sock.sendall(data)
+            data = self.receive_data(len(data))
+            print data
+        except:
+            print 'error in send_data'
+
+    def end_connection(self):
+        print >>sys.stderr, 'closing socket'
+        self.sock.close()
+
+def test():
+    client = Client()
+    x = '+'
+    for i in range(1, 8):
+        x += '+'
+        client.send_data(x)
+    client.end_connection()
+
+def __main__():
+    # client = Client()
+    # client.send_data(raw_input())
+    test()
+
+__main__()
--- tp/tp0/server.py
+++ tp/tp0/server.py
@@ -1,47 +1,79 @@
-from struct import *
 import socket
+import sys
+
+HOST = ''
+PORT = 5000
 
 class Server(object):
-    """docstring for Server."""
-    def __init__(self, **kwargs):
-        self.host = kwargs.get('host', '')
-        self.port = kwargs.get('port', 5000)
-        self.counter = kwargs.get('counter', 0)
-        self.max_counter = kwargs.get('max', 999)
 
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host', HOST)
+        self.port = kwargs.get('port', PORT)
+        self.counter = kwargs.get('counter', 0)
+        # Cria o socket TCP/IP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection = None
+        self.address = None
 
     def add_counter(self):
         self.counter += 1
-        self.counter = self.counter % (self.max_counter + 1)
         return self.counter
 
     def sub_counter(self):
         self.counter -= 1
-        self.counter = self.counter % (self.max_counter + 1)
         return self.counter
 
-    def start(self):
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tcp.bind((self.host, self.port))
-        tcp.listen(1)
-        while True:
-            connection, cliente = tcp.accept()
-            print 'Conectado por', cliente
-            while True:
-                msg = connection.recv(1)
-                if msg == '+':
-                    self.add_counter()
-                elif msg == '-':
-                    self.sub_counter()
-                elif msg == 'q':
-                    print 'Finalizando conexao do cliente', cliente
-                    connection.close()
-                    return True
-                if not msg: break
-                print cliente, msg
-                print 'Counter = ', self.counter
-            print 'Finalizando conexao do cliente', cliente
-            connection.close()
+    def parse_str(self, data):
+        balance = 0
+        for c in data:
+            if c == '+':
+                self.add_counter()
+                balance += 1
+            elif c == '-':
+                self.sub_counter()
+                balance -= 1
+        return balance
 
-x = Server()
-x.start()
+    def start_connection(self):
+        # Bind the socket to the port
+        server_address = (self.host, self.port)
+        print >>sys.stderr, 'starting up on %s por %s' % server_address
+        self.sock.bind(server_address)
+        # Listen for incoming connections
+        self.sock.listen(1)
+
+        while True:
+            # wait for a connections
+            print >>sys.stderr, 'waiting for a connection'
+            self.connection, self.client_address = self.sock.accept()
+
+            try:
+                print >>sys.stderr, 'connection from', self.client_address
+                #Receive the data in small chunks and retransmit it
+                while True:
+                    data = self.connection.recv(16)
+                    print >>sys.stderr, 'received "%s"' % data
+                    self.parse_str(data)
+                    if data:
+                        print >>sys.stderr, 'sending data back to the client'
+                        self.connection.sendall(data)
+                    else:
+                        print >>sys.stderr, 'no more data from', self.client_address
+                        break
+            finally:
+                # Clean up the connection
+                self.close_connection()
+                print 'Counter', self.counter
+
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.close()
+            return True
+        else:
+            return False
+
+def __main__():
+    server = Server()
+    server.start_connection()
+
+__main__()
