b6779942d23043766d4a5779d0dad4ea9a5c2b84
ignitz <ignitzhjfk@gmail.com>
2017-04-06 15:13:33 -0300

    Step

--- tp/tp0/src/c/client/client.c
+++ tp/tp0/src/c/client/client.c
@@ -9,51 +9,56 @@
 
 #define h_addr h_addr_list[0] /* for backward compatibility */
 
+#define PORT 51515
+
 void error(const char *msg)
 {
-  perror(msg);
-  exit(0);
+    perror(msg);
+    exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-  int sockfd, portno, n;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-  char buffer[256];
-  if (argc < 3) {
-    fprintf(stderr,"usage %s hostname port\n", argv[0]);
-    exit(0);
-  }
-  portno = atoi(argv[2]);
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-    error("ERROR opening socket");
-  server = gethostbyname(argv[1]);
-  if (server == NULL) {
-    fprintf(stderr,"ERROR, no such host\n");
-    exit(0);
-  }
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr,
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
-  serv_addr.sin_port = htons(portno);
-  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-    error("ERROR connecting");
-  printf("Please enter the message: ");
-  bzero(buffer,256);
-  fgets(buffer,255,stdin);
-  n = write(sockfd,buffer,strlen(buffer));
-  if (n < 0)
-    error("ERROR writing to socket");
-  bzero(buffer,256);
-  n = read(sockfd,buffer,255);
-  if (n < 0)
-    error("ERROR reading from socket");
-  printf("%s\n",buffer);
-  close(sockfd);
-  return 0;
+    char buffer[255];
+
+    if (argc < 2) {
+        fprintf(stderr,"usage %s <inc/dec>\n", argv[0]);
+        exit(0);
+    }
+    portno = PORT;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname("localhost");
+
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    bzero(buffer,256);
+    buffer[0] = '+';
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+        error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
 }
